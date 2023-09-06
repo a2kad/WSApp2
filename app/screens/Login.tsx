2f00956c8e1703cacf0ee10c8263417302656ca3
a2kad <reshetnikovruslan705@gmail.com>
2023-09-06 16:33:49 +0200

    init commit

---
+++ app/screens/Login.tsx
@@ -0,0 +1,83 @@
+import { View, Text, Platform, StyleSheet, TextInput, ActivityIndicator, Button, KeyboardAvoidingView } from 'react-native'
+import React, { useState } from 'react'
+import { FIREBASE_AUTH } from '../../FirebaseConfig';
+import {signInWithEmailAndPassword, createUserWithEmailAndPassword} from 'firebase/auth'
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+    const auth = FIREBASE_AUTH;
+
+    const signIn = async () => {
+        setLoading(true);
+        try{
+            const reponse = await signInWithEmailAndPassword(auth, email,password);
+            console.log(reponse);
+            
+        } catch (error: any) {
+            console.log(error);
+            alert('Sign In faled : '+error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const signUp = async () => {
+        setLoading(true);
+        try{
+            const reponse = await createUserWithEmailAndPassword(auth, email,password);
+            console.log(reponse);
+            alert('Check your emails!');
+        } catch (error:any) {
+            console.log(error);
+            alert('Sign Up faled : '+error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return (
+    
+    <View style={styles.container}>
+        <View style={styles.top} >
+            <Text>
+            Bienvenue sur WebSolidarité App
+            </Text>
+        </View>
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <TextInput value={email} style={styles.input} placeholder='Email' autoCapitalize='none' onChangeText={(text) => setEmail(text)}></TextInput>
+        <TextInput secureTextEntry={true} value={password} style={styles.input} placeholder='Password' autoCapitalize='none' onChangeText={(text) => setPassword(text)}></TextInput>
+        {
+            loading ? <ActivityIndicator size="large" color="#0000ff" />
+            : <>
+            <Button title='Login' onPress={signIn} />
+            <Button title='Create Account' onPress={signUp} />
+            </>
+        }
+        </KeyboardAvoidingView>
+    </View>
+    )
+}
+
+export default Login
+
+const styles = StyleSheet.create({
+    top: {
+        marginHorizontal:20,
+
+    },
+    container: {
+        marginHorizontal: 20,
+        flex: 1,
+        justifyContent:'center',
+    },
+    input: {
+        marginVertical: 4,
+        height:50,
+        borderWidth:1,
+        borderRadius: 4,
+        padding: 10,
+        backgroundColor: '#fff',
+    }
+})
