b65303207e414325aa4fc957a1f7c52c1b9237f2
a2kad <reshetnikovruslan705@gmail.com>
2023-09-08 14:03:13 +0200

    New FirebaseConfig

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { initializeAuth } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-import { getAuth } from "firebase/auth";
 import * as firebaseAuth from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
